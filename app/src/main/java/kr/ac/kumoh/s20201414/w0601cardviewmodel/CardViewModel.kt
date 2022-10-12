7841f3ec9c2aeabfffabe316d0bdfa0cade7ac18
jkc-mycode <jkc9709@naver.com>
2022-10-12 15:56:54 +0900

    :rocket: Update Source

---
+++ app/src/main/java/kr/ac/kumoh/s20201414/w0601cardviewmodel/CardViewModel.kt
@@ -0,0 +1,23 @@
+package kr.ac.kumoh.s20201414.w0601cardviewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class CardViewModel: ViewModel() {
+    private val _cards = MutableLiveData<IntArray>()
+    val cards: LiveData<IntArray>
+        get() = _cards
+
+    val random = Random(System.currentTimeMillis());
+
+    fun deal(){
+        val c = IntArray(5)
+
+        for(i in c.indices){
+            c[i] = random.nextInt(52)
+            _cards.value = c
+        }
+    }
+}
--- app/src/main/java/kr/ac/kumoh/s20201414/w0601cardviewmodel/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w0601cardviewmodel/MainActivity.kt
@@ -3,43 +3,25 @@ package kr.ac.kumoh.s20201414.w0601cardviewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import kr.ac.kumoh.s20201414.w0601cardviewmodel.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val model: CardViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
-//        val c = Random.nextInt(52)
-//        Log.i("Test", "$c : ${getCardName(c)}")
-//
-//        val res = resources.getIdentifier(
-//            getCardName(c),
-//            "drawable",
-//            packageName
-//        )
-//
-//        binding.card1.setImageResource(res)
-        binding.btnDeal.setOnClickListener {
-            val c = IntArray(5)
+        model.cards.observe(this, Observer<IntArray>{
             val res = IntArray(5)
 
-            //for (i in 0..4)
-            //for (i in 0 until 5)
-            //for (i in 0 until c.size)
-            for (i in c.indices) {
-                c[i] = Random.nextInt(52)
-
-                Log.i("Test", "${c[i]} : " +
-                        "${getCardName(c[i])}")
-
+            for (i in it.indices) {
                 res[i] = resources.getIdentifier(
-                    getCardName(c[i]),
+                    getCardName(it[i]),
                     "drawable",
                     packageName
                 )
@@ -51,6 +33,10 @@ class MainActivity : AppCompatActivity() {
             binding.card3.setImageResource(res[2])
             binding.card4.setImageResource(res[3])
             binding.card5.setImageResource(res[4])
+        })
+
+        binding.btnDeal.setOnClickListener {
+            model.deal()
         }
     }
 
